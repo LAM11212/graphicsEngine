e367687f60be9ef4b6c82da65ecaecfcd309339d
levi <leviamullins@gmail.com>
2025-05-18 01:11:10 -0400

    Lift your skinny fists like antennas to heaven

--- openGL/main.cpp
+++ openGL/main.cpp
@@ -27,20 +27,21 @@ struct Tile {
 
 std::vector<Tile> placedTiles;
 
-void writeToFile(const std::vector<Tile>& allTiles);
-void readFromFile(const std::string fileName);
+void writeToFile(const std::vector<Tile>& allTiles, std::string fileName);
+void readFromFile(std::vector<Tile>& allTiles, const std::string fileName);
 //********************************************************************
 //                         GLOBAL VARS
 //********************************************************************
 const int tileSize = 16;
 bool buildMode = false;
 bool verticesChanged = false;
-const float blockSize = 0.2;
+const float blockSize = 0.1;
 float currentX = -0.5f;
 float currentY = -0.5f;
 float UVSize = 1.0f / tileSize;
 const int MAX_VERTS = 1000 * 5 * 6;
-
+bool pKeyPressed = false;
+bool lKeyPressed = false;
 //********************************************************************
 //                          Tile Enum
 //********************************************************************
@@ -52,7 +53,9 @@ enum tileType {
     Coal,
     Iron,
     Gravel,
-    Sand
+    Sand,
+    Water,
+    Lava
 };
 tileType selectedTile = Side;
 //********************************************************************
@@ -63,16 +66,35 @@ tileType selectedTile = Side;
 // BE CONVERTED TO (0, 15) I.E (just add 15 to the y value)
 // WE DO THIS BECAUSE IF UNFLIPPED THE TEXTURE RENDERS UPSIDE DOWN.
 
-
-
 const int sideX = 1;     // grass block side
 const int sideY = 15;    // grass block side
 
 const int grassX = 0;    // full grass block
 const int grassY = 15;   // full grass block
 
-const int dirtX = 3;     // dirt block
+const int dirtX = 2;     // dirt block
 const int dirtY = 15;    // dirt block
+
+const int stoneX = 3;    // stone block
+const int stoneY = 15;   // stone block
+
+const int coalX = 4;     // coal block
+const int coalY = 15;    // coal block
+
+const int ironX = 5;     // iron block
+const int ironY = 15;    // iron block
+
+const int gravelX = 6;   // gravel block
+const int gravelY = 15;  // gravel block
+
+const int sandX = 0;     // sand block
+const int sandY = 14;    // sand block
+
+const int waterX = 0;    // water block
+const int waterY = 0;    // water block
+
+const int lavaX = 0;     // lava block
+const int lavaY = 1;     // lava block
 
 UV calculateUV(int coordX, int coordY)
 {
@@ -83,16 +105,14 @@ UV calculateUV(int coordX, int coordY)
     return { uMin, uMax, vMin, vMax };
 }
 
-
-
 //********************************************************************
-//                         initial vertices
+//                         initial vertice
 //********************************************************************
 
 std::vector<float> makeBaseTile(float blockSize, float uMin, float uMax, float vMin, float vMax)
 {
     return {
-        // x, y, z, u, v
+        //       x                   y         z      u     v
         -0.5f * blockSize, -0.5f * blockSize, 0.0f, uMin, vMin,
          0.5f * blockSize, -0.5f * blockSize, 0.0f, uMax, vMin,
          0.5f * blockSize,  0.5f * blockSize, 0.0f, uMax, vMax,
@@ -220,7 +240,6 @@ int main(void)
             
         }
         
-        
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -230,8 +249,6 @@ int main(void)
 
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture);
-
-        
 
         if (!verticeVector.empty()) {
             glDrawArrays(GL_TRIANGLES, 0, verticeVector.size() / 5);
@@ -256,10 +273,30 @@ void getBuildMode(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS)
     {
-        writeToFile(placedTiles);
+        if (!pKeyPressed)
+        {
+            writeToFile(placedTiles, "data.txt");
+        }
+        pKeyPressed = true;
+    }
+    else
+    {
+        pKeyPressed = false;
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS)
+    {
+        if (!lKeyPressed)
+        {
+            readFromFile(placedTiles, "data.txt");
+        }
+        lKeyPressed = true;
+    }
+    else
+    {
+        lKeyPressed = false;
     }
     
-
     if (glfwGetKey(window, GLFW_KEY_G) == GLFW_PRESS)
     {
         buildMode = true;
@@ -296,8 +333,30 @@ bool processInput(GLFWwindow* window)
     {
         selectedTile = Stone;
     }
-    
-    
+    else if (glfwGetKey(window, GLFW_KEY_5))
+    {
+        selectedTile = Coal;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_6))
+    {
+        selectedTile = Iron;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_7))
+    {
+        selectedTile = Gravel;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_8))
+    {
+        selectedTile = Sand;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_9))
+    {
+        selectedTile = Water;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_0))
+    {
+        selectedTile = Lava;
+    }
     
     if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS && !mouseHeld)
     {
@@ -315,10 +374,37 @@ bool processInput(GLFWwindow* window)
         {
             uv = calculateUV(dirtX, dirtY);
         }
+        else if (selectedTile == Stone)
+        {
+            uv = calculateUV(stoneX, stoneY);
+        }
+        else if (selectedTile == Coal)
+        {
+            uv = calculateUV(coalX, coalY);
+        }
+        else if (selectedTile == Iron)
+        {
+            uv = calculateUV(ironX, ironY);
+        }
+        else if (selectedTile == Gravel)
+        {
+            uv = calculateUV(gravelX, gravelY);
+        }
+        else if (selectedTile == Sand)
+        {
+            uv = calculateUV(sandX, sandY);
+        }
+        else if (selectedTile == Water)
+        {
+            uv = calculateUV(waterX, waterY);
+        }
+        else if (selectedTile == Lava)
+        {
+            uv = calculateUV(lavaX, lavaY);
+        }
 
         baseTile = makeBaseTile(blockSize, uv.uMin, uv.uMax, uv.vMin, uv.vMax);
         
-        std::cout << "left click detected" << std::endl;
         glfwGetCursorPos(window, &xpos, &ypos);
 
         int width, height;
@@ -329,7 +415,6 @@ bool processInput(GLFWwindow* window)
         float snappedX = floor(ndcX / blockSize) * blockSize + blockSize / 2.0f;
         float snappedY = floor(ndcY / blockSize) * blockSize + blockSize / 2.0f;
 
-        std::cout << "snappedX: " << snappedX << "snappedY: " << snappedY << std::endl;
 
         Tile newTile;
         newTile.position = { snappedX, snappedY };
@@ -402,27 +487,79 @@ bool processInput(GLFWwindow* window)
 //********************************************************************
 //                         File I/O
 //********************************************************************
-// WE WORKING ON THIS ONE. 
-void writeToFile(const std::vector<Tile>& allTiles)
-{
-    for (int i = 0; i < allTiles.size(); i++)
-    {
-        const Tile& newTile = allTiles[i];
-        std::cout << "Tile at position (" << newTile.position.x << ", " << newTile.position.y << ")\n";
 
-        for (int j = 0; j < newTile.vertices.size(); j += 5)
+void writeToFile(const std::vector<Tile>& allTiles, std::string fileName)
+{
+    std::ofstream file;
+    file.open(fileName);
+
+    if (file.is_open())
+    {
+        for (int i = 0; i < allTiles.size(); i++)
         {
-            std::cout << "  Vertex " << j / 5 << ": "
-                << "x=" << newTile.vertices[j + 0] << ", "
-                << "y=" << newTile.vertices[j + 1] << ", "
-                << "z=" << newTile.vertices[j + 2] << ", "
-                << "u=" << newTile.vertices[j + 3] << ", "
-                << "v=" << newTile.vertices[j + 4] << '\n';
+            const Tile& newTile = allTiles[i];
+            for (int j = 0; j < newTile.vertices.size(); j += 5)
+            {
+                file 
+                    << newTile.vertices[j + 0] << ", "
+                    << newTile.vertices[j + 1] << ", "
+                    << newTile.vertices[j + 2] << ", "
+                    << newTile.vertices[j + 3] << ", "
+                    << newTile.vertices[j + 4] << '\n';
+            }
+        }
+        file.close();
+        std::cout << "data written successfully" << std::endl;
+    }
+    else {
+        std::cout << "failed to open file." << std::endl;
+    } 
+}
+
+void readFromFile(std::vector<Tile>& allTiles, const std::string fileName)
+{
+    std::ifstream file(fileName);
+    if (!file.is_open())
+    {
+        std::cout << "failed to opne file." << std::endl;
+        return;
+    }
+
+    std::string line;
+    std::vector<float> tempVerts;
+    while (std::getline(file, line))
+    {
+        std::stringstream ss(line);
+        std::string val;
+        float f;
+
+        for (int i = 0; i < 5; i++)
+        {
+            std::getline(ss, val, ',');
+            try 
+            {
+                f = std::stof(val);
+                tempVerts.push_back(f);
+            }
+            catch (std::exception e)
+            {
+                std::cout << e.what() << std::endl;
+                break;
+            }
+        }
+
+        if (tempVerts.size() == 30)
+        {
+            Tile newTile;
+            newTile.vertices = tempVerts;
+
+            newTile.position.x = newTile.vertices[0];
+            newTile.position.y = newTile.vertices[1];
+
+            allTiles.push_back(newTile);
+            tempVerts.clear();
         }
     }
-}
-// WE ALSO WORKING ON THIS ONE.
-void readFromFile(const std::string fileName)
-{
-
+    file.close();
+    std::cout << "loaded " << allTiles.size() << " tile correctly. yahooo!" << std::endl;
 }
